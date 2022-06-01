b3d11686820a12959abcfea8abaecf8d72ae0436
berkanankal <berkan.ankal@outlook.com>
2022-06-01 13:49:43 +0300

    MongoDB connection with mongoose

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./config/env/config.env" });
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running on port ${PORT} | ${process.env.NODE_ENV}`)
+    )
+  )
+  .catch((err) => console.error(err));
