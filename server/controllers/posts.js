f30ba70b1f3d2ba466f18a2b60e446f116d0adab
berkanankal <berkan.ankal@outlook.com>
2022-06-01 15:02:30 +0300

    Created post model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,5 @@
+const Post = require("../models/Post");
+
 const getAllPosts = (req, res) => {
   res.send("Get All Posts");
 };
---
+++ server/models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
