75efacc10abaa52e4b06bacef4fa0c9b1390d886
berkanankal <berkan.ankal@outlook.com>
2022-07-19 09:24:21 +0300

    Bug fixed

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -52,6 +52,7 @@ const Auth = () => {
     email: Yup.string()
       .email("Please enter a valid email")
       .required("Email is required"),
+    password: Yup.string().required("Password is required"),
   });
 
   const {
